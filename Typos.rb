45e4fffb1c4d159942f1521859162ce91dae2737
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-21 17:19:18 +0100

    Typos file added and typos fixed

---
+++ Typos.rb
@@ -0,0 +1,21 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #Create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #Get another name from the user
+    name = gets.chomp
+    end
+    #return the array of students
+  students
+end
+
+
+puts input_students
