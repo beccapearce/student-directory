10b3089762a67f4cb773de2ac7406b97c2dd3546
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-21 11:11:00 +0100

    cohorts added and the name and cohort printed as symbols in the print names method

--- directory.rb
+++ directory.rb
@@ -1,33 +1,33 @@
 #Outline the names of the students
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"ALex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Kruger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohot: :november},
+{name: "ALex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Kruger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november},
 ]
 #A method to introduce our list of students
 def print_header
-puts "The students of Villains Academy"
-puts "-------------"
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
 
 #A method to print out the names of the students
-def print_names(names)
-  names.each do |name|
-    puts name
+def print_names(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 #A method to show the overall number of students from the list
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
 print_header
