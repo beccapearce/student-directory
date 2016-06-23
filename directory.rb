e7df4e6260cce48105b3e9a779d31ea8dadb4aac
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-06-23 11:54:26 +0100

    Added section to save the student list to a new file

--- directory.rb
+++ directory.rb
@@ -1,27 +1,6 @@
-def interactive_menu
-  students = []
-  loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-     when "1"
-      students = input_students
-     when "2"
-      print_header
-      print_student(students)
-      print_footer(students)
-     when "9"
-       exit
-     else
-      puts "I don't know what you meant, try again"
-    end
-   end
-end
+@students = []
 
 def input_students
-  students = []
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   name = gets.chomp
@@ -31,29 +10,65 @@ def input_students
     if cohort.empty?
       cohort = "Unknown".to_sym
     end
-    students << {name: name, cohort: cohort}
-    if students.length < 2
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort}
+    if @students.length < 2
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
      puts "Please enter the next students name"
      name = gets.chomp
   end
-   if students.length < 1
+   if @students.length < 1
      puts "No students where entered"
      exit
    else
-     students
+     @students
    end
 end
 
- def print_header
-   puts "The students of Makers Academy".center(50)
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+   end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
+ def process(selection)
+   case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+     puts "I don't know what you mean, try again"
+   end
  end
 
- def print_student(students)
-   cohort_sorted = students.group_by { |hash| hash[:cohort] }
+ def print_header
+   puts "The students of Makers Academy"
+   puts "----------------"
+ end
+
+ def print_students_list(students)
+   cohort_sorted = @students.group_by { |hash| hash[:cohort] }
    cohort_sorted.each do |k,v|
      puts "Cohort #{k}"
      v.each do |student|
@@ -64,6 +79,16 @@ end
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(", ")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
